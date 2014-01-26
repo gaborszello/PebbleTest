47ab7c742e2a2990b5a5e74f0d86df1dfe62d51e
Gábor Szellő <gabor.szello@gmail.com>
2014-01-26 09:33:59 -0800

    Minor changes

--- src/main.c
+++ src/main.c
@@ -3,12 +3,33 @@
 Window *my_window;
 TextLayer *my_text_layer;
 
+void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+		vibes_short_pulse();
+		int pia = rand() % 4;
+		if(pia == 0)
+                text_layer_set_text(my_text_layer, "SÖRT");
+        else if(pia == 1)
+                text_layer_set_text(my_text_layer, "BORT");
+		else if(pia == 2)
+                text_layer_set_text(my_text_layer, "PÁLINKÁT");
+		else if(pia == 3)
+                text_layer_set_text(my_text_layer, "KOKTÉLT");
+		else if(pia == 4)
+                text_layer_set_text(my_text_layer, "PEZSGŐT");
+}
+
+void click_config_provider(void *context) {
+        window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+}
+
 void window_load(Window *my_window) {
 	// Itt adunk tartalmat az ablaknak.
-	my_text_layer = text_layer_create(GRect(0, 0, 144, 168));
+	my_text_layer = text_layer_create(GRect(0, 55, 144, 168));
 	text_layer_set_background_color(my_text_layer, GColorClear);
 	text_layer_set_text_color(my_text_layer, GColorBlack);
- 
+	text_layer_set_text_alignment(my_text_layer, GTextAlignmentCenter);
+	text_layer_set_font(my_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+
 	layer_add_child(window_get_root_layer(my_window), (Layer*) my_text_layer);
 	text_layer_set_text(my_text_layer, "Mit igyak?");
 }
@@ -26,6 +47,7 @@ void init() {
 		.unload = window_unload,
 	});
 	
+	window_set_click_config_provider(my_window, click_config_provider);
 	window_stack_push(my_window, true);
 }
 
